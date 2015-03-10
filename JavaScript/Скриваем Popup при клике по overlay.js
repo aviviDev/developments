166b80b3d69b346702e106eceb26b9b7360a32ad
Dmitro <pokefrost@gmail.com>
2015-03-10 11:53:23 +0200

    Dmitro Simonv

---
+++ JavaScript/Скриваем Popup при клике по overlay.js
@@ -0,0 +1,12 @@
+$('body').on("mousedown touchend", function (e)
+			{
+			    var container = $(selector); //Селектор Popupa
+			    
+
+			    if (!container.is(e.target) // if the target of the click isn't the container...
+			        && container.has(e.target).length === 0) // ... nor a descendant of the container
+			    {
+			        container.hide();
+			       
+			    }
+			});
---
+++ JavaScript/скриваем PopUp.js
@@ -0,0 +1,14 @@
+  // общая функция скрытия
+        function hide() {
+            $('.overlay, .block-call').fadeOut(300, 0);
+        }
+        // закрытие по клавише Esc
+        $('body').keyup(function(e) {
+            if (e.keyCode == 27) {
+                hide();
+            }
+        });
+        $('.overlay, .close').click(function() {
+            hide();
+            return false;
+        });
