05b0c65709ac21cf26b37012306d5304405a9a47
Dmitro <pokefrost@gmail.com>
2015-03-10 11:57:56 +0200

    Dmitro Simonov

--- JavaScript/скриваем PopUp.js
+++ JavaScript/скриваем PopUp.js
@@ -2,12 +2,14 @@
         function hide() {
             $('.overlay, .block-call').fadeOut(300, 0);
         }
+        
         // закрытие по клавише Esc
         $('body').keyup(function(e) {
             if (e.keyCode == 27) {
                 hide();
             }
         });
+
         $('.overlay, .close').click(function() {
             hide();
             return false;
